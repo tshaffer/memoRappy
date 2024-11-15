c1521f99236d64e75e3b37617148bdfca570e4dc
Ted Shaffer <shaffer.family@gmail.com>
2024-11-15 06:59:01 -0800

    Remove unused top level navigation buttons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
 import ReviewForm from './pages/ReviewForm';
-import ViewReviews from './pages/ViewReviews';
-import QueryReviews from './pages/QueryReviews';
 import Reviews from './pages/Reviews';
 import './App.css';
 
@@ -16,16 +14,12 @@ const App: React.FC = () => {
             MemoRapp
           </Typography>
           <Button color="inherit" component={Link} to="/">Reviews</Button>
-          <Button color="inherit" component={Link} to="/view-reviews">View Reviews</Button>
-          <Button color="inherit" component={Link} to="/query-reviews">Query Reviews</Button>
           <Button color="inherit" component={Link} to="/add-review">Add Review</Button>
         </Toolbar>
       </AppBar>
       <Container className="full-width-container">
         <Routes>
           <Route path="/" element={<Reviews />} />
-          <Route path="/view-reviews" element={<ViewReviews />} />
-          <Route path="/query-reviews" element={<QueryReviews />} />
           <Route path="/add-review" element={<ReviewForm />} />
           <Route path="/add-review/:_id" element={<ReviewForm />} />
         </Routes>
